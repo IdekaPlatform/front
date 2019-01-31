d768ac0d842b997a73542a11403bb8757fcbda80
thomas <tp456@hotmail.fr>
2019-01-31 20:55:54 +0100

    google and twitter login

--- pages/index.vue
+++ pages/index.vue
@@ -28,12 +28,17 @@
                             <v-card-actions>
                                 <v-btn color="secondary" to="/signup">Inscription</v-btn>
                                 <v-spacer></v-spacer>
+                                <v-btn @click="googleLogIn" class="loginButton google"><v-icon left color="#0057e7">fab fa-google</v-icon>Se connecter</v-btn>
+                                <v-spacer></v-spacer>
+                                <v-btn @click="twitterLogIn" class="loginButton twitter">Se connecter<v-icon right color="#1da1f2">fab fa-twitter</v-icon></v-btn>
+                                <v-spacer></v-spacer>
                                 <v-btn color="primary" type="submit" :disabled="!valid" @click="login">Login</v-btn>
                             </v-card-actions>
                         </v-card>
 </template>
 
 <script>
+    import firebase from 'firebase'
     export default {
       layout: 'login',
       data: () => ({
@@ -57,7 +62,43 @@
           if (this.valid) {
             this.$router.push('/home')
           }
+        },
+        googleLogIn () {
+          this.googleProvider = new firebase.auth.GoogleAuthProvider()
+          firebase.auth().signInWithPopup(this.googleProvider).then(result => {
+            console.log(result)
+            this.$router.push('/home')
+          }).catch(e => {
+            this.$snotify.error(e.message)
+            console.log(e)
+          })
+        },
+        twitterLogIn () {
+          this.twtterProvider = new firebase.auth.TwitterAuthProvider()
+          firebase.auth().signInWithPopup(this.twtterProvider).then(result => {
+            console.log(result)
+            this.$router.push('/home')
+          }).catch(e => {
+            console.log(e)
+          })
         }
       }
     }
 </script>
+
+<style>
+
+    .loginButton {
+        background-color: transparent !important;
+        border: 1px solid currentColor;
+        box-shadow: none;
+    }
+
+    .google {
+        color: #0057e7 !important;
+    }
+
+    .twitter {
+        color: #1da1f2 !important;
+    }
+</style>
